ccf30dbe5c5b26ea984afa86b57ec846d74eedb0
hazimabhar <hazimabhar123@gmail.com>
2023-10-25 15:20:24 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,9 +10,7 @@
                 />
             </div>
             <div class="user-icon">
-                <Icon :icon="$router.options.routes[1].children[0].icon" class="text-[56px] mr-10 cursor-pointer"
-                @click=alwaysOpen
-                />
+                <Icon :icon="$router.options.routes[1].children[0].icon" class="text-[56px] mr-10 cursor-pointer"/>
                 <p v-if="role === 'manager'"
                     class="font-semibold text-[18px] origin-left cursor-pointer"
                     :class="{'disappear': !isOpen}"
@@ -24,65 +22,60 @@
                     >Worker
                 </p>
             </div>
-            <div class="navigation-option" @click="openOtherMenu" >
-                <Icon :icon="$router.options.routes[1].icon" class="option-icon" 
-                @click=alwaysOpen
-                />
-                <router-link
-                    :to="{ name: 'Manager'}"
-                    type="button"
-                    class="option-text"
-                    :class="{'disappear': !isOpen}"
-                    >
-                    {{ $router.options.routes[1].name}}
-                </router-link>            
-            </div>
-            <div class="flex items-center justify-between" @click="openInventory">
+            <router-link 
+            :to="{ name: 'Manager'}"
+            type="button"
+            class="option-background"
+            @click="openOtherMenu">
                 <div class="navigation-option">
-                    <Icon :icon="$router.options.routes[1].children[2].icon" class="option-icon" 
-                    @click=alwaysOpen
-                    />
-                    <router-link
-                        :to="{ name: 'Inventory'}"
-                        type="button"
-                        class="option-text"
-                        :class="{'disappear': !isOpen}"
-                        >
-                        {{ $router.options.routes[1].children[2].name}}
-                    </router-link>            
+                    <Icon :icon="$router.options.routes[1].icon" class="option-icon"/>
+                    <div class="option-text" :class="{'disappear': !isOpen}">
+                        {{ $router.options.routes[1].name}}
+                    </div>            
                 </div>
+            </router-link>
+            <div class="option-background" @click="openInventory">
+                <router-link 
+                :to="{ name: 'Inventory'}"
+                type="button"
+                class="option-background">
+                        <div class="navigation-option">
+                            <Icon :icon="$router.options.routes[1].children[2].icon" class="option-icon"/>
+                            <div class="option-text" :class="{'disappear': !isOpen}">
+                                {{ $router.options.routes[1].children[2].name}}
+                            </div>            
+                        </div>
+                </router-link>
                 <div>
                     <Icon icon="subway:down-2" class="text-[15px] mr-8 cursor-pointer" :class="{'disappear': !isOpen} "/>
                 </div>
             </div>
             <div v-for="child in $router.options.routes[1].children[2].children" :key="child.name" class="sub-menu" :class="{ 'close-submenu': !inventoryDropown  }">
-                 <div class="flex">
+                <div class="flex">
                     <div class="option-icon" :class="{ 'close-submenu': !isOpen }" >
                         <Icon :icon="child.icon"/>
                     </div>
                     <div class="option-text" :class="{ 'close-submenu': !isOpen }" >
                         {{ child.name }}
                     </div>
-                 </div>   
+                </div> 
             </div>
-                <div class="flex items-center justify-between" @click="openCatalogue">
+            <div class="option-background" @click="openCatalogue">
+                <router-link 
+                :to="{ name: 'Catalogue'}"
+                type="button"
+                class="option-background" >
                     <div class=" navigation-option">
-                    <Icon :icon="$router.options.routes[1].children[3].icon" class="option-icon"
-                    @click=alwaysOpen
-                    />
-                        <router-link
-                            :to="{ name: 'Catalogue'}"
-                            type="button"
-                            class="option-text"
-                            :class="{'disappear': !isOpen}"
-                            >
+                    <Icon :icon="$router.options.routes[1].children[3].icon" class="option-icon"/>
+                        <div class="option-text" :class="{'disappear': !isOpen}">
                             {{ $router.options.routes[1].children[3].name}}
-                        </router-link>      
+                        </div>      
                     </div>
-                    <div>
-                        <Icon icon="subway:down-2" class="text-[15px] mr-8 cursor-pointer" :class="{'disappear': !isOpen}"/>
-                    </div>
+                </router-link>
+                <div>
+                    <Icon icon="subway:down-2" class="text-[15px] mr-8 cursor-pointer" :class="{'disappear': !isOpen}"/>
                 </div>
+            </div>
                 <div v-for="child in $router.options.routes[1].children[3].children" :key="child.name" class="sub-menu" :class="{ 'close-submenu': !catalogueDropown  }">
                     <div class="flex">
                         <div class="option-icon" :class="{ 'close-submenu': !isOpen }" >
@@ -94,60 +87,56 @@
                     </div>   
                 </div>
             <div v-if="role === 'manager'">
-                <div class="navigation-option" @click="openOtherMenu" >
-                    <Icon :icon="$router.options.routes[1].children[6].icon" class="option-icon" 
-                    @click=alwaysOpen
-                    />
-                    <router-link
-                        :to="{ name: 'User'}"
-                        type="button"
-                        class="option-text"
-                        :class="{'disappear': !isOpen}"
-                        >
-                        {{ $router.options.routes[1].children[6].name}}
-                    </router-link>            
-                </div>
-                <div class="navigation-option" @click="openOtherMenu" >
-                    <Icon :icon="$router.options.routes[1].children[7].icon" class="option-icon" 
-                    @click=alwaysOpen
-                    />
-                    <router-link
-                        :to="{ name: 'Sales Report'}"
-                        type="button"
-                        class="option-text"
-                        :class="{'disappear': !isOpen}"
-                        >
-                        {{ $router.options.routes[1].children[7].name}}
-                    </router-link>            
-                </div>
+                <router-link
+                :to="{ name: 'User'}"
+                type="button"
+                 class="option-background"
+                 @click="openOtherMenu">
+                    <div class="navigation-option">
+                        <Icon :icon="$router.options.routes[1].children[6].icon" class="option-icon"/>
+                        <div class="option-text" :class="{'disappear': !isOpen}">
+                            {{ $router.options.routes[1].children[6].name}}
+                        </div>            
+                    </div>
+                </router-link>
+                <router-link 
+                :to="{ name: 'Sales Report'}"
+                type="button"
+                class="option-background"
+                @click="openOtherMenu">
+                    <div class="navigation-option">
+                        <Icon :icon="$router.options.routes[1].children[7].icon" class="option-icon"/>
+                        <div class="option-text" :class="{'disappear': !isOpen}">
+                            {{ $router.options.routes[1].children[7].name}}
+                        </div>            
+                    </div>
+                </router-link>
             </div>
             <div v-else>
-                <div class="navigation-option" @click="openOtherMenu" >
-                    <Icon :icon="$router.options.routes[1].children[4].icon" class="option-icon"
-                    @click=alwaysOpen
-                    />
-                    <router-link
-                        :to="{ name: 'Cashier'}"
-                        type="button"
-                        class="option-text"
-                        :class="{'disappear': !isOpen}"
-                        >
-                        {{ $router.options.routes[1].children[4].name}}
-                    </router-link>            
-                </div>
-                <div class="navigation-option" @click="openOtherMenu" >
-                    <Icon :icon="$router.options.routes[1].children[5].icon" class="option-icon" 
-                    @click=alwaysOpen
-                    />
-                    <router-link
-                        :to="{ name: 'Closing'}"
-                        type="button"
-                        class="option-text"
-                        :class="{'disappear': !isOpen}"
-                        >
-                        {{ $router.options.routes[1].children[5].name}}
-                    </router-link>            
-                </div>
+                <router-link 
+                :to="{ name: 'Cashier'}"
+                type="button"
+                class="option-background"
+                @click="openOtherMenu">
+                    <div class="navigation-option">
+                        <Icon :icon="$router.options.routes[1].children[4].icon" class="option-icon"/>
+                        <div class="option-text" :class="{'disappear': !isOpen}">
+                            {{ $router.options.routes[1].children[4].name}}
+                        </div>            
+                    </div>
+                </router-link>
+                <router-link 
+                :to="{ name: 'Closing'}"
+                type="button"
+                class="option-background"
+                @click="openOtherMenu">
+                    <div class="navigation-option">
+                        <Icon :icon="$router.options.routes[1].children[5].icon" class="option-icon"/>
+                        <div class="option-text" :class="{'disappear': !isOpen}">
+                            {{ $router.options.routes[1].children[5].name}}
+                        </div>            
+                    </div>
+                </router-link>
             </div>
         </div>
         <div class="p-10">
@@ -181,18 +170,18 @@ export default{
             this.inventoryDropown = false
             this.catalogueDropown = false
         },
-        alwaysOpen(){
-            this.isOpen = true
-        },
         openInventory(){
+            this.isOpen = true
             this.inventoryDropown = !this.inventoryDropown
             this.catalogueDropown = false
         },
         openCatalogue(){
+            this.isOpen = true
             this.catalogueDropown = !this.catalogueDropown
             this.inventoryDropown = false
         },
         openOtherMenu(){
+            this.isOpen = true
             this.catalogueDropown = false
             this.inventoryDropown = false
         },
